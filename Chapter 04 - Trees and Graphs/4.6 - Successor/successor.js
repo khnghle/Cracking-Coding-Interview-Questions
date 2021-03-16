c1f236134cdeb84d879bdf54cb27b87dc5bdad40
Khuong <lekhuongy@gmail.com>
2021-03-16 12:22:20 -0500

    4.5 and 4.6

---
+++ Chapter 04 - Trees and Graphs/4.6 - Successor/successor.js
@@ -0,0 +1,37 @@
+const BST = require("../utils/BST");
+
+function successor(targetNode, root) {
+  const stack = [];
+  let currNode = root;
+
+  while (true) {
+    if (currNode.value === targetNode.value) {
+      if (currNode.right) {
+        //go all the way to the left
+        currNode = currNode.right;
+        while (currNode.left) currNode = currNode.left;
+        return currNode;
+      } else if (stack.length) {
+        return stack.pop();
+      } else break;
+    } else if (currNode.value > targetNode.value) {
+      stack.push(currNode);
+      if (currNode.left) currNode = currNode.left;
+      else break; // if val does not exist
+    } else {
+      if (currNode.right) currNode = currNode.right;
+      else break; // if val does not exist
+    }
+  }
+  return console.error(
+    "you have found the largest value or the value doesn't exist"
+  );
+}
+
+const newBST = new BST();
+const testCase = [3, 1, 7, 0, 2, 5, 10, 4, 6, 8, 9];
+newBST.addArray(testCase);
+
+const givenNode = newBST.findVal(-1);
+
+console.log(successor(givenNode, newBST.getTree()));
