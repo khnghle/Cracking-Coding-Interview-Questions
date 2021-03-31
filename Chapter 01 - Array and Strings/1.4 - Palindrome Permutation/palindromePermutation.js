c3011449df6330f08e2ec8b10b66efba26c4cba2
qi lin <qlin1996@gmail.com>
2021-03-31 11:46:40 -0400

    added palindromePermutation

--- Chapter 01 - Array and Strings/1.4 - Palindrome Permutation/palindromePermutation.js
+++ Chapter 01 - Array and Strings/1.4 - Palindrome Permutation/palindromePermutation.js
@@ -1,3 +1,28 @@
-function palindromePermutation () {
+function palindromePermutation(string) {
+  let charCounter = 0;
+  let memo = {};
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== " ") {
+      charCounter++;
+      if (!memo[string[i]]) memo[string[i]] = 1;
+      else memo[string[i]]++;
+    }
+  }
+  let seenOdd = 0;
 
+  for (let key in memo) {
+    if (memo[key] % 2) seenOdd++;
+    // if charCounter is even
+    if (!(charCounter % 2) && seenOdd) return false;
+    // if charCounter is odd
+    if (charCounter % 2 && seenOdd > 1) return false;
+  }
+
+  return true;
 }
+
+const test1 = palindromePermutation("tact coa");
+const test2 = palindromePermutation("a dad");
+const test3 = palindromePermutation("pizza pie");
+
+console.log({ test1, test2, test3 });
