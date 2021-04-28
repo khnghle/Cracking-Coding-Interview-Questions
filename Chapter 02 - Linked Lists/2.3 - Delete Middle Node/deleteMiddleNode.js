9133c1e784296e413d5c147d545c1837ef483718
qi lin <qlin1996@gmail.com>
2021-04-28 15:44:36 -0400

    added 2.3

---
+++ Chapter 02 - Linked Lists/2.3 - Delete Middle Node/deleteMiddleNode.js
@@ -0,0 +1,16 @@
+const LinkedList = require("../utils/SinglyLL");
+
+function deleteMiddleNode(deletedNode) {
+  if (!deletedNode.next) return false;
+
+  const nextNode = deletedNode.next.next;
+  deletedNode.value = deletedNode.next.value;
+  deletedNode.next = nextNode;
+}
+
+const LL = new LinkedList();
+LL.addArray(["a", "b", "c", "d", "e", "f"]);
+
+const node = LL.findValue("null");
+deleteMiddleNode(node);
+LL.traverseTree();
